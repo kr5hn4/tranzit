5f95c4643b3fbd54ca58e750bcdf8c4eacc9c3a9
kr5hn4 <cod3ncoff33@gmail.com>
2025-08-12 01:44:05 +0530

    feat: Use single-instance plugin to run only single instance of the app at a time

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,6 +21,9 @@ static APP_HANDLE: OnceCell<AppHandle> = OnceCell::new();
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub async fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+            // Write your code here...
+        }))
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
