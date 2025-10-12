e703134f3969ad92f50d5c2c4d487db2921c52f8
kr5hn4 <cod3ncoff33@gmail.com>
2025-10-12 17:11:10 +0530

    refactor: convert inline code into function and add tsdoc comments

--- src/lib/utils/sfx.ts
+++ src/lib/utils/sfx.ts
@@ -1,25 +1,52 @@
-function isMp3PlaybackSupported(type = "audio/mpeg") {
+/**
+ * Check if mp3 playback is supported on the browser/webview
+ * @param type     - MIME type of the file to check for playback support
+ * @returns          True if supported, else false
+ */
+function isMp3PlaybackSupported(type = "audio/mpeg"): boolean {
   const audio = document.createElement("audio");
   return !!audio.canPlayType && audio.canPlayType(type) !== "";
 }
 
-let popSound: HTMLAudioElement;
-let successSound: HTMLAudioElement;
+type sfx = {
+  [key: string]: HTMLAudioElement;
+};
 
-// load sounds only once
-if (isMp3PlaybackSupported()) {
-  //pre-load pop sound
-  popSound = new Audio("/sfx/pop.mp3");
-  popSound.preload = "auto";
-  popSound.load(); // starts loading immediately
+let { popSound, successSound }: sfx = loadSoundFiles();
 
-  //pre-load success sound
-  successSound = new Audio("/sfx/success.mp3");
-  successSound.preload = "auto";
-  successSound.load();
+/**
+ * Load all the sfx files
+ * @returns  An object containing all the supported sfx
+ */
+function loadSoundFiles(): sfx {
+  let popSound: HTMLAudioElement;
+  let successSound: HTMLAudioElement;
+
+  // load sounds only once
+  if (isMp3PlaybackSupported()) {
+    //pre-load pop sound
+    popSound = new Audio("/sfx/pop.mp3");
+    popSound.preload = "auto";
+    popSound.load(); // starts loading immediately
+
+    //pre-load success sound
+    successSound = new Audio("/sfx/success.mp3");
+    successSound.preload = "auto";
+    successSound.load();
+    return { popSound, successSound };
+  } else {
+    popSound = new Audio();
+    successSound = new Audio();
+    return { popSound, successSound };
+  }
 }
 
-export function playSfx(name: string) {
+/**
+ * Play the given sfx if sounds are enabled in settings
+ * @param name     - name of the sfx to be played (there are 2 currently supported i.e "pop" and "success")
+ * @returns          void
+ */
+export function playSfx(name: string): void {
   const isSfxEnabled = localStorage.getItem("isSfxEnabled") ?? "false";
   if (isMp3PlaybackSupported() && isSfxEnabled === "true") {
     const pop = popSound.cloneNode() as HTMLAudioElement;
